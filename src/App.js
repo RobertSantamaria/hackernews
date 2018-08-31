3e037f7f2eb6ed68f84feefc5ad61b181b9f276c
roberto Algarra <ralgarra@illumina.com>
2018-08-31 15:28:36 +0100

    ES6 Destructuring

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -38,12 +40,13 @@ class App extends Component {
     this.setState({ searchTerm: event.target.value });
   }
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
         <form>
           <input type="text" onChange={this.onSearchChange}/>
         </form>
-        {this.state.list.map(item =>
+        {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
