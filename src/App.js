b3005170f5ff5b129b33583c2529dda135c4ea64
roberto Algarra <ralgarra@illumina.com>
2018-09-06 14:32:45 +0100

    ES6 Spread Operators

--- src/App.js
+++ src/App.js
@@ -1,64 +1,87 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+const Search = ({ value, onChange, children }) => {
+  return (
+    <form>
+        {children}
+        <input type="text" value={value} onChange={onChange}/>
+    </form>
+  );
+}
+
+const Table = ({ list, pattern, onDismiss }) =>
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID} className="table-row">
+        <span className="largeColumn">
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span className="midColumn">{item.author}</span>
+        <span className="smallColumn">{item.num_comments}</span>
+        <span className="smallColumn">{item.points}</span>
+        <span className="smallColumn">
+          <Button onClick={() => onDismiss(item.objectID)} className="button-inline">
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
+
+const Button = ({ onClick, className='', children }) => {
+  return (
+    <button type="button" onClick={onClick} className={className}>
+      {children}
+    </button>
+  );
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
-    this.onDismiss = this.onDismiss.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  setSearchTopStories(result) {
+    this.setState({result});
+  }
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error);
   }
   onDismiss(id) {
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updatedList });
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({ result: {...this.state.result, hits: updatedHits }});
   }
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    if (!result) { return null; }
     return (
-      <div className="App">
-        <form>
-          <input type="text" onChange={this.onSearchChange}/>
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button type="button" onClick={() => this.onDismiss(item.objectID)}>Dismiss</button>
-            </span>
-          </div>
-        )}
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange}>Search</Search>
+        </div>
+        <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
   }
