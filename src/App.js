bab6a02960ff445882597741ef846f081d2eafef
roberto Algarra <ralgarra@illumina.com>
2018-09-06 15:14:57 +0100

    Client-Server

--- src/App.js
+++ src/App.js
@@ -6,20 +6,17 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 
-const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
-const Search = ({ value, onChange, children }) => {
-  return (
-    <form>
-        {children}
+const Search = ({ value, onChange, onSubmit, children }) =>
+    <form onSubmit={onSubmit}>
         <input type="text" value={value} onChange={onChange}/>
+        <button type="submit">
+          {children}
+        </button>
     </form>
-  );
-}
 
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span className="largeColumn">
           <a href={item.url}>{item.title}</a>
@@ -52,6 +49,7 @@ class App extends Component {
       searchTerm: DEFAULT_QUERY,
     };
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
@@ -59,12 +57,15 @@ class App extends Component {
   setSearchTopStories(result) {
     this.setState({result});
   }
-  componentDidMount() {
-    const { searchTerm } = this.state;
+  fetchSearchTopStories(searchTerm) {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
     .catch(error => error);
+  }
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
@@ -74,17 +75,19 @@ class App extends Component {
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
-  onSearchSubmit() {
+  onSearchSubmit(event) {
     const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
   }
   render() {
     const { searchTerm, result } = this.state;
     return (
       <div className="page">
         <div className="interactions">
-          <Search value={searchTerm} onChange={this.onSearchChange}>Search</Search>
+          <Search value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>Search</Search>
         </div>
-        { result && <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} /> }
+        { result && <Table list={result.hits} onDismiss={this.onDismiss} /> }
       </div>
     );
   }
